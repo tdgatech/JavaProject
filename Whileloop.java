48415766198914b03d7e420afd09c96b47960dc9
Toan Duong <tduong41@gatech.edu>
2018-09-30 15:08:30 -0400

    update

--- Whileloop.java
+++ Whileloop.java
@@ -6,21 +6,31 @@ public class Whileloop{
 	public static void main(String[]args){
 		int number = 1;
 		int finish = 20;
+		int count = 0;
+		int totalsum = 0;
 		while (number <= 20){
 			number++;
 			if(!isEvenNumber(number)){
 				continue;
-			}else{
-				System.out.println("Even number is " + number);
-			}
-		}
-		System.out.println();		
-		for(number = 1; number <= finish ; number++){
-			if(!isEvenNumber(number)){
-				continue;
-			}
+			}		
+			
 			System.out.println("Even number is " + number);
+			totalsum += number;
+			count++;
+			if(count >= 5){
+				break;
+			}
+			
 		}
+		System.out.println(count);
+		System.out.println(totalsum);
+		//System.out.println();		
+		// for(number = 1; number <= finish ; number++){
+		// 	if(!isEvenNumber(number)){
+		// 		continue;
+		// 	}
+		// 	System.out.println("Even number is " + number);
+		// }
 	}
 	// create a method called is Evennunber that tajkes a aparameter of type int
 	// its purpose is to determinent if the argument passes to the method is
