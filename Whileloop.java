53c78c259673740f1dfdebae3605413b83f74ca5
Toan Duong <tduong41@gatech.edu>
2018-09-30 14:18:20 -0400

    even number code has added using while loop

--- Whileloop.java
+++ Whileloop.java
@@ -1,5 +1,21 @@
 public class Whileloop{
 	public static void main(String[]args){
-
+		int number = 1;
+		int finish = 20;
+		while (number <= 20){
+			number++;
+			if(!isEvenNumber(number)){
+				continue;
+			}else{
+				System.out.println("Even number is " + number);
+			}
+		}
+	}
+	public static boolean isEvenNumber(int number){
+		if(number % 2 == 0){
+			return true;
+		}else{
+			return false;
+		}
 	}
 }
