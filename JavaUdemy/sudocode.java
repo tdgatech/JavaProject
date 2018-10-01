7d95ad1445c1d0859f5a355612575d89c4706b39
Toan Duong <tduong41@gatech.edu>
2018-09-30 21:03:40 -0400

    Javaudemy folder dekleted

--- JavaUdemy/sudocode.java
+++
@@ -1,8 +0,0 @@
-import java.util.Arrays;
-import java.util.Scanner;
-public class sudocode{
-	public static void main(String[] args){
-		String [] arr = {"english.corpus:", "french.corpus", "spanish.corpus", "hiphop.corpus", "lisp.corpus" };
-		System.out.println(Arrays.toString(arr));
-	}
-}
