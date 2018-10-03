d362e3a26c2b0470ec486af5f0fee17f438141b2
Toan Duong <tduong41@gatech.edu>
2018-10-02 23:57:46 -0400

    share digit solved

---
+++ ComparetheTriplets.java
@@ -0,0 +1,6 @@
+public class ComparetheTriplets{
+	public static int compareTriplets
+	public static void main(String[] args){
+
+	}
+}
--- ShareDigit.java
+++ ShareDigit.java
@@ -1,17 +1,26 @@
 public class ShareDigit{
-	private static boolean hasShareDigit(int num1, int num2){
-	 	if((num1<10 || num1>99)||(num2<10 || num2>99)){
+	private static boolean hasSharedDigit(int num1, int num2){
+	 	if(num1 < 10 || num1 > 99){
 	 		return false;
-	 	}else if((num1%10 == num2/10)){
-	 		return true;
-	 	}else if(num1/10 == num2%10){
-	 		return true;
+	 	}else if (num2 < 10 || num2 > 99){
+	 		return false;
 	 	}
-	 	return false;
+	 	int lastDigitNum1 = num1%10;
+	 	int firstDigitNum1 = num1/10;
+	 	while (num2 > 0){
+	 		int num2Digit = num2 % 10;
+	 		if (lastDigitNum1 == num2Digit){
+	 			return true;
+	 		}else if(firstDigitNum1 == num2Digit){
+	 			return true;// can add to if to make code shorter but not a big deal lol
+	 		}
+	 		num2/=10;
+
+	 	}return false;
 
 	}	
 	public static void main(String[] args){
-	 	boolean result = hasShareDigit(12,23);
+	 	boolean result = hasSharedDigit(15,55);
 	 	System.out.println(result);
 	 }
 }
