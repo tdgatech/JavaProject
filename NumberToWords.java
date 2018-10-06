d02bc75ecc93a008a8d30c992abe4e703ad9d7c9
Toan Duong <tduong41@gatech.edu>
2018-10-06 16:49:16 -0400

    Nunber To Word file procedded but need to run test key

--- NumberToWords.java
+++ NumberToWords.java
@@ -1,57 +1,74 @@
 import java.lang.*;
+import java.util.Arrays;
 public class NumberToWords{
-	public static void nunberToWords(int number1){
+	public static void numberToWords(int number1){
 		if(number1 < 0){
 			System.out.println("Invalid Value");
 		}
-	}
-	public static int getReverse(int number2){		
-		
-		if(number2 < 0){
-			//System.out.println(number2);//test
-			int absolute = Math.abs(number2);
-			System.out.println(absolute);//test
-			//System.out.println(Math.abs(number2));//test
-			int sum = 0;
-			int firstDigit = 0;
-			if(absolute % 10 == 0){
-				firstDigit+= (absolute%10);
-
+		String[] words = {"","one","two","three","four","five","six","seven","eight","nine"};
+		while(number1 > 0){
+			int transfer = number1 % 10;
+			if(transfer == 0){
+				System.out.println("zero");
+			}else{
+				System.out.println(words[transfer]);
 			}
-			while((absolute % 10) > 0){//100/10;10/10;1/10fail
-				if((absolute % 10 == 0)){
-				//int firstDigit = absolute/10;
-				int reverse1 = absolute % 10;//mod1=0;mod2=0;
-								
-				
-				sum = (sum*10 + reverse1 + firstDigit);//sum1=0;sum2-=0;
-				absolute/=10;
-				}
-			}//aabsolute / 10)bd=10;abs=1;
-			sum *=(-1);
-			return (sum);
+			number1/=10;
 
-		
+
+		}
+
+	}
+	public static int reverse(int number2){
+		//String[] words = {"","one","two","three","four","five","six","seven","eight","nine"};			
+		if(number2 < 0){			
+			int absolute = Math.abs(number2);
+			int sum = 0;						
+			while(absolute > 0){
+				int reverse1 = absolute % 10;
+				// if(reverse1==0){
+				// 	System.out.println("zero");
+				// }else{
+				// 	System.out.println(words[reverse1]);
+				// }				
+				sum = sum*10 + reverse1 ;
+				absolute/=10;			
+			}
+			sum *=(-1);
+			return (sum);		
 		}else{
 			int sum = 0;
-			//System.out.println(number2);//test
-			
-			while ( (number2 / 10) >= 0 ){
-				//System.out.println(number2);//test
-				
+			while (number2 > 0 ){
 				int reverse2 = number2 % 10;
-				//System.out.println(reverse2);//test
+				// if(reverse2 == 0){
+				// 	System.out.println("zero");
+				// }else{
+				// 	System.out.println(words[reverse2]);
+				// }				
 				sum = sum*10 + reverse2;
 				number2/=10;
-				//System.out.println(sum);//test
 			}
-			//System.out.println(number2)
 			return sum;
 		}						
 	}
+	public static int getDigitCount(int number3){
+		if (number3 < 0){
+			return -1;
+		}
+		int count = 0;
+		while(number3 > 0){
+			count+=1;
+			number3/=10;
+		}
+		return count;
+
+	}
+
 	public static void main (String[] args){
-		nunberToWords(-1);
-		int reverse = getReverse(-100);
+		numberToWords(1000);
+		int reverse = reverse(-121);
 		System.out.println(reverse);
+		int getDigit = getDigitCount(5200);
+		System.out.println("Number of digits are: " + getDigit);
 	}
 }
